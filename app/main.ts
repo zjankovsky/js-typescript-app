05340b1ca6fe0000e83cdf4848b83b2fb01664a4
Zdeněk Jankovský <zjankovsky@arcdata.cz>
2019-10-24 10:54:24 +0200

    Přidání widgetu Search a vyhledání kongresového centra

--- app/main.ts
+++ app/main.ts
@@ -1,5 +1,6 @@
 import EsriMap from "esri/Map";
 import MapView from "esri/views/MapView";
+import Search from "esri/widgets/Search";
 
 const map = new EsriMap({
   basemap: "topo"
@@ -10,4 +11,13 @@ const view = new MapView({
   container: "viewDiv",
   center: [-118.244, 34.052],
   zoom: 12
+});
+
+
+const search = new Search({
+  view: view
+});
+view.ui.add(search, "top-right");
+view.when(function() {
+  search.search("Kongresové centrum Praha, Česká republika");
 });
