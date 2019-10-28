93687decdbb579a88efaca4d3fc4a46689730d5d
Zdeněk Jankovský <zjankovsky@arcdata.cz>
2019-10-28 17:27:38 +0100

    Vykreslování souřadnice SJTSK pomocí funkce project počítané na klientovi

--- src/widgets/ShowCoordinates.tsx
+++ src/widgets/ShowCoordinates.tsx
@@ -9,6 +9,13 @@ import { renderable, tsx } from "esri/widgets/support/widget";
 import Widget from "esri/widgets/Widget";
 import MapView from "esri/views/MapView";
 
+import GeographicTransformationStep from "esri/geometry/support/GeographicTransformationStep";
+import GeographicTransformation from "esri/geometry/support/GeographicTransformation";
+import * as projection from "esri/geometry/projection";
+import * as webMercatorUtils from "esri/geometry/support/webMercatorUtils"
+// viz https://github.com/Esri/arcgis-webpack-plugin/issues/41
+import { Point, SpatialReference } from "esri/geometry";
+
 import ShowCoordinatesViewModel from "./ShowCoordinates/ShowCoordinatesViewModel";
 
 interface ShowCoordinatesConstructorParams {
@@ -24,12 +31,48 @@ export default class ShowCoordinates extends declared(Widget) {
 
   roundPrec:number = 100;
 
+  wgsToSjtskTrans:GeographicTransformation = new GeographicTransformation({
+    steps: [
+      new GeographicTransformationStep({
+        isInverse: true,
+        wkid: 1623
+      })
+    ]
+  });
+
+  sjtskSpRef:SpatialReference = new SpatialReference({
+    wkid: 5514
+  });
+
+  isProjectionLoaded:boolean = false;
+
   constructor(params: ShowCoordinatesConstructorParams) {
     super();
 
     this.viewModel =  new ShowCoordinatesViewModel({
       mapView: params.mapView
     });
+
+    this.webMercatToSjtsk = this.webMercatToSjtsk.bind(this);
+
+    if(!projection.isSupported()){
+      console.warn("This web browser does not support projection on the client.");
+      this.isProjectionLoaded = false;
+    }
+    else if(!projection.isLoaded()){
+      console.log("Projection is not loadded yet!");
+
+      projection.load().then(() => {
+        console.log("Projection is loaded right now!");
+        this.isProjectionLoaded = true;
+      }).catch((err)=> {
+        console.error("Projection loading ends with error!", err);
+        this.isProjectionLoaded = false;
+      });
+    }
+    else {
+      this.isProjectionLoaded = true;
+    }
   }
 
   @property({
@@ -48,16 +91,41 @@ export default class ShowCoordinates extends declared(Widget) {
   @aliasOf("viewModel.yCoord")
   yCoord:number;
 
+  webMercatToSjtsk(coords:number[],
+    sjtskSpRef:SpatialReference = this.sjtskSpRef,
+    transfr:GeographicTransformation = this.wgsToSjtskTrans,
+    isProjectionLoaded:boolean = this.isProjectionLoaded):number[] {
+   
+    if(!coords || coords.length != 2 || !coords[0] || !coords[1] || !isProjectionLoaded)
+      return null;
+
+    const wgs84:number[] = webMercatorUtils.xyToLngLat(coords[0], coords[1]);
+    const sjtskPoint:Point = projection.project(new Point({x: wgs84[0], y:wgs84[1]}), sjtskSpRef , transfr) as Point;
+
+    return [sjtskPoint.x, sjtskPoint.y];
+  }
+
   render() {
 
     const x:number = Math.round(this.xCoord * this.roundPrec) / this.roundPrec;
     const y:number = Math.round(this.yCoord * this.roundPrec) / this.roundPrec;
 
+    const sjtsk:number[] = this.webMercatToSjtsk([x, y]);
+    let pSjtsk = null;
+    if(sjtsk) {
+      pSjtsk = (
+        <p key="sjstskCoords">
+          {sjtsk[0].toFixed(2)} ; {sjtsk[1].toFixed(2)}
+        </p>
+      );
+    }
+
     return (
       <div class={CSS.base}>
-        <p>
+        <p key="wmCoords">
           {x.toFixed(2)} ; {y.toFixed(2)}
         </p>
+        {pSjtsk}
       </div>
     );
   }
